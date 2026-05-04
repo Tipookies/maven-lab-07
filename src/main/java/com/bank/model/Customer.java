d1cf4a100865506fc3f25534f3970b7e3de04a42
Thangdaichim <danhthang783@gamil.com>
2026-05-04 15:56:32 +0700

    add lỗi checkstyle

--- src/main/java/com/bank/model/Customer.java
+++ src/main/java/com/bank/model/Customer.java
@@ -123,4 +123,6 @@ public class Customer {
   public String getCustomerInfo() {
     return "Số CMND: " + idNumber + ". Họ tên: " + fullName + ".";
   }
+
+    public void test(){System.out.println("hello xin chao");}
 }
